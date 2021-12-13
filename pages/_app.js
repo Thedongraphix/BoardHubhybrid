854cbeda39047be50cd8d4d11d2f8accbf6a8268
thanhlam <lam.nguyen@pascal.studio>
2021-12-14 01:51:46 +0700

    add google analytic

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
+import Router from "next/router";
+import ReactGA from "react-ga";
+
 import "react-multi-carousel/lib/styles.css";
 import "react-modal-video/css/modal-video.min.css";
 import "rc-drawer/assets/index.css";
 import "typeface-dm-sans";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    ReactGA.initialize("UA-164554931-1");
+    Router.events.on("routeChangeComplete", () =>
+      ReactGA.pageview(window.location.pathname + window.location.search)
+    );
+  }, []);
   return <Component {...pageProps} />;
 }
 
